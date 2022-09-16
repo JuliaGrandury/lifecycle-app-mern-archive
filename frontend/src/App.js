a8d055c3551f1f91362a97df9a52a78ddf0785db
juliagrandury <juliagrandury@wustl.edu>
2022-09-16 12:09:32 -0400

    Page structure and header initial creation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
 function App() {
   return (
-    <div>
-      <h1>Heyo Badass</h1>
-    </div>
+    <>
+      <Router>
+        <div className='container'>
+          <Header />
+          {/* Can't have anything other than routes in the routes tags */}
+          <Routes>
+            <Route path='/' element={<Dashboard />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
