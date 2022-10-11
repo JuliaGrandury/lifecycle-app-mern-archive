b2df9ea1ceb088343a1a0284e8d15aa25e2ab5b6
juliagrandury <juliagrandury@wustl.edu>
2022-10-11 16:13:54 -0400

    Added multiselect fields, working on form UI

--- frontend/src/components/ItemComp.jsx
+++ frontend/src/components/ItemComp.jsx
@@ -11,8 +11,8 @@ function ItemComp({ item }) {
             <h3>Color: {item.color}</h3>
             <h3>Category: {item.category}</h3>
             <h3>Season: {item.season}</h3>
-            <h5>In Closet: {item.inCloset ? "True" : "False"}</h5>
-            <h5>To Repair: {item.toRepair ? "True" : "False"}</h5>
+            <>{item.inCloset ? (<div className="greentag">In Closet</div>) : (<div className="orangetag">Out of Closet</div>)}</>
+            <>{item.toRepair ? (<div>To Repair</div>) : (<></>)}</>
             <button onClick={() => dispatch(deleteItem(item._id))} className="close">X</button>
         </div>
     )
--- frontend/src/components/ItemForm.jsx
+++ frontend/src/components/ItemForm.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { createItem } from '../features/items/itemSlice';
 import styles from './Modal.module.css';
+import morestyles from './ItemForm.module.css';
 import { RiCloseLine } from "react-icons/ri";
 
 function ItemForm({ setIsOpen }) {
@@ -32,10 +33,6 @@ function ItemForm({ setIsOpen }) {
         //             <input type="text" name="category" id="category" placeholder='Select item category' value={category} onChange={(e) => setCategory(e.target.value)} />
         //             <input type="text" name="season" id="season" placeholder='Select item season' value={season} onChange={(e) => setSeason(e.target.value)} />
         //         </div>
-        //         <div className="form-actions">
-        //             <button className="btn btn-block" type="submit">Save</button>
-        //             <button className="btn btn-block" type="submit">Clear</button>
-        //         </div>
         //     </form>
         // </section>
         <>
@@ -44,15 +41,40 @@ function ItemForm({ setIsOpen }) {
                 <div className={styles.modal}>
                     <div className={styles.modalHeader}><h5 className={styles.heading}>Add an Item</h5></div>
                     <button className={styles.closeBtn} onClick={() => setIsOpen(false)}><RiCloseLine style={{ marginBottom: "-3px" }} /></button>
-                    <div className={styles.modalContent}>
-                        {/* <ItemForm /> */}
-                    </div>
-                    <div className={styles.modalActions}>
-                        <div className={styles.actionsContainer}>
-                            <button className={styles.saveBtn} onClick={() => setIsOpen(false)}>Save</button>
-                            <button className={styles.clearBtn} onClick={() => setIsOpen(false)}>Clear</button>
+
+                    <form onSubmit={onSubmit}>
+                        <div className={morestyles.formgroup}>
+                            <input className={morestyles.formgroupInput} type="text" name="name" id="name" placeholder='Enter item name' value={name} onChange={(e) => setName(e.target.value)} />
+                            <input type="text" name="color" id="color" placeholder='Enter item color' value={color} onChange={(e) => setColor(e.target.value)} />
+
+                            <select name="category" id="category" value={category} onChange={(e) => setCategory(e.target.value)}>
+                                <option value="" disabled selected hidden>Select item category</option>
+                                <option value="tops">Tops</option>
+                                <option value="bottoms">Bottoms</option>
+                                <option value="dressesandjumpsuits">Dresses + Jumpsuits</option>
+                                <option value="coatsandjackets">Coats + Jackets</option>
+                                <option value="shoes">Shoes</option>
+                                <option value="accessories">Accessories</option>
+                                <option value="sportswear">Sportswear</option>
+                            </select>
+
+                            <select name="season" id="season" value={season} onChange={(e) => setSeason(e.target.value)}>
+                                <option value="" disabled selected hidden>Select item season</option>
+                                <option value="hot">Hot</option>
+                                <option value="temperate">Temperate</option>
+                                <option value="cold">Cold</option>
+                                <option value="all">All</option>
+                            </select>
+
+                            {/* <input type="text" name="category" id="category" placeholder='Select item category' value={category} onChange={(e) => setCategory(e.target.value)} /> */}
+                            {/* <input type="text" name="season" id="season" placeholder='Select item season' value={season} onChange={(e) => setSeason(e.target.value)} /> */}
                         </div>
-                    </div>
+                        <div className='form-group'>
+                            <button className={styles.saveBtn} type="submit" onClick={() => setIsOpen(false)}>Add</button>
+                            <button className={styles.clearBtn} type="reset">Clear</button>
+                        </div>
+                    </form>
+
                 </div>
             </div>
         </>
