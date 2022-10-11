9eacdfc1e01a6276e2a46c20017436ff416cb6bb
juliagrandury <juliagrandury@wustl.edu>
2022-10-11 00:15:38 -0400

    Fixed Boolean handling

--- frontend/src/components/ItemComp.jsx
+++ frontend/src/components/ItemComp.jsx
@@ -6,13 +6,13 @@ function ItemComp({ item }) {
 
     return (
         <div className="item">
-            <h5>{new Date(item.createdAt).toLocaleString('en-US')}</h5>
+            {/* <h5>{new Date(item.createdAt).toLocaleString('en-US')}</h5> */}
             <h2>{item.name}</h2>
             <h3>Color: {item.color}</h3>
             <h3>Category: {item.category}</h3>
             <h3>Season: {item.season}</h3>
-            <h5>In Closet: {item.incloset ? "True" : "False"}</h5>
-            <h5>To Repair: {item.torepair ? "True" : "False"}</h5>
+            <h5>In Closet: {item.inCloset ? "True" : "False"}</h5>
+            <h5>To Repair: {item.toRepair ? "True" : "False"}</h5>
             <button onClick={() => dispatch(deleteItem(item._id))} className="close">X</button>
         </div>
     )
